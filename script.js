1f9991186241c35cdf98947548c29821bbe4e56d
alovett0829 <alexis.n.rubio@gmail.com>
2022-01-25 22:21:32 -0600

    Add the grid

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const container = document.getElementById('grid');
+const clear = document.getElementById('clear');
+let size = 75;
 
-for (let i = 0; i < 16; i++) {
-        const cell = document.createElement("div");
-        container.appendChild(cell);
-        cell.classList.add("cell-style");
+function makeGrid (size) {
+        let sizeSecondPower = Math.pow(size, 2);
+        console.log(sizeSecondPower);
+
+        for (let i = 0; i < sizeSecondPower; i++) {
+                const cell = document.createElement("div");
+                container.appendChild(cell);
+                cell.classList.add("cell-style");
+        }
+
+        container.style['grid-template-columns'] = `repeat(${size}, 1fr)`;
+        container.style['grid-template-rows'] = `repeat(${size}, 1fr)`;
 }
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
         let createdCells = document.getElementsByClassName('cell-style');
@@ -14,6 +26,19 @@ document.addEventListener("DOMContentLoaded", function () {
                         createdCells[i].classList.add('change');
                 });
         }
+        
+        function reset () {
+                for (let i = 0; i < createdCells.length; i++) {
+                        createdCells[i].classList.remove('change');
+                }
+        }
+
+        clear.addEventListener('click', reset);
 });
+
+makeGrid(size);
+
+
+
 
 

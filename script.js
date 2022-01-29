af6fe22df9a1e5c04387aac542b7ad6bd4366818
alovett0829 <alexis.n.rubio@gmail.com>
2022-01-29 15:57:23 -0600

    Finish js file, add arrow functions

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const container = document.getElementById('grid');
-const clear = document.getElementById('clear');
-const createdCells = document.getElementsByClassName('cell-style');
-const label = document.getElementById('size');
+const createdCells = document.getElementsByClassName('created-cell');
+const sizeLabel = document.getElementById('size-label');
 const pickColor = document.getElementById('pick-color');
+const rainbow = document.getElementById('rainbow-color');
 const gridLines = document.getElementById('grid-lines');
 const eraser = document.getElementById('eraser');
-const rainbow = document.getElementById('rainbow-color');
+const clear = document.getElementById('clear');
 
 let squares = 16;
 let sizeSecondPower = 256;
+
+function makeGrid (squares, sizeSecondPower) {
+
+        for (let i = 0; i < sizeSecondPower; i++) {
+                const cell = document.createElement("div");
+                container.appendChild(cell);
+                cell.classList.add("created-cell");
+        }
+
+        container.style['grid-template-columns'] = `repeat(${squares}, 1fr)`;
+        container.style['grid-template-rows'] = `repeat(${squares}, 1fr)`;
+}
 
 function getValue (squares, sizeSecondPower) {
 
@@ -19,29 +31,11 @@ function getValue (squares, sizeSecondPower) {
                 container.removeChild(container.firstChild);
         }
 
-        label.innerText = `${squares} X ${squares}`;
+        sizeLabel.innerText = `${squares} X ${squares}`;
 
         makeGrid(squares, sizeSecondPower);
         changeColor();
 };
-
-function makeGrid (squares, sizeSecondPower) {
-
-        for (let i = 0; i < sizeSecondPower; i++) {
-                const cell = document.createElement("div");
-                container.appendChild(cell);
-                cell.classList.add("cell-style");
-        }
-
-        container.style['grid-template-columns'] = `repeat(${squares}, 1fr)`;
-        container.style['grid-template-rows'] = `repeat(${squares}, 1fr)`;
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-        changeColor();  
-        reset();
-        clear.addEventListener('click', reset);
-});
 
 function changeColor () {
         for (let i = 0; i < createdCells.length; i++) {
@@ -51,33 +45,16 @@ function changeColor () {
         }
 }
 
-function reset () {
-        for (let i = 0; i < createdCells.length; i++) {
-                createdCells[i].style.cssText = "background-color: ligth gray";
-        }
-}
-
-
-eraser.addEventListener('click', function () {
-        for (let i = 0; i < createdCells.length; i++) {
-                createdCells[i].addEventListener('mouseover', () => {
-                        createdCells[i].style.cssText = "background-color: light gray";
-                });
-        }
-});
-
-pickColor.addEventListener('input', function (e) {
+pickColor.addEventListener('input', (e) => {
         let newColor = e.target.value;
         for (let i = 0; i < createdCells.length; i++) {
                 createdCells[i].addEventListener('mouseover', () => {
                         createdCells[i].style.cssText = `background-color: ${newColor}`;
                 });
         }
-
 });
 
-rainbow.addEventListener('click', function () {
-
+rainbow.addEventListener('click', () => {
         for (let i = 0; i < createdCells.length; i++) {
                 createdCells[i].addEventListener('mouseover', () => {
 
@@ -86,15 +63,33 @@ rainbow.addEventListener('click', function () {
                         let b = Math.floor(Math.random() * 256);;
 
                         createdCells[i].style.cssText = `background-color: rgb(${r}, ${g}, ${b})`;
-
                 });
         }       
 });
 
-gridLines.addEventListener('click', function () {
+gridLines.addEventListener('click', () => {
         for (let i = 0; i < createdCells.length; i++) {
                 createdCells[i].classList.toggle('grid-lines');
         }
+});
+
+
+eraser.addEventListener('click', () => {
+        for (let i = 0; i < createdCells.length; i++) {
+                createdCells[i].addEventListener('mouseover', () => {
+                        createdCells[i].style.cssText = "background-color: light gray";
+                });
+        }
+});
+
+clear.addEventListener('click', () => {
+        for (let i = 0; i < createdCells.length; i++) {
+                createdCells[i].style.cssText = "background-color: light gray";
+        }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+        changeColor();  
 });
 
 makeGrid (squares, sizeSecondPower);
